ea40c886ca3a6b82047a0d463cfcf6864a94197b
shimonmagal <shimon.magal@takipi.com>
2017-11-06 17:37:53 +0200

    jdbcdown

---
+++ db/jdbcdown/index.js
@@ -0,0 +1,186 @@
+'use strict';
+var jdbc = require('trireme-jdbc');
+var inherits = require('inherits');
+var AbstractLevelDOWN = require('abstract-leveldown').AbstractLevelDOWN;
+var Iter = require('./iterator');
+var fs = require('fs');
+var Promise = require('bluebird');
+var url = require('url');
+// Todo: break to one meta-table for tables and separate tables for each dynamo table.
+// Currently, everything is on one table
+var TABLENAME = 'takipi_jdbcdown';
+var debug = require('debug')('jdbcdown');
+var util = require('./encoding');
+module.exports = JDBCdown;
+
+inherits(JDBCdown, AbstractLevelDOWN);
+
+function JDBCdown(jdbcUrl, jdbcUser, jdbcPassword) {
+    AbstractLevelDOWN.call(this, jdbcUrl);
+
+    var pool = connection(jdbcUrl, jdbcUser, jdbcPassword);
+    this.pool = pool;
+    this.tablename = TABLENAME;
+}
+
+JDBCdown.prototype._open = function(options, callback) {
+    this.tablename = TABLENAME;
+
+    var tableCreateStr = 'CREATE TABLE IF NOT EXISTS ' + this.tablename + '(' +
+        'K VARCHAR(3072) NOT NULL, V BLOB, PRIMARY KEY(K)) ' +
+        'ENGINE=InnoDB';
+
+    this.pool.execute(tableCreateStr,
+        function(err, result) {
+            callback();
+        });
+}
+
+JDBCdown.destroy = function(location, options, callback) {
+    if (typeof options === 'function') {
+        callback = options;
+        options = {};
+    }
+    // unsupported so we don't accidently destory our data:
+    //
+    // this.pool.execute('drop table if exists ' + this.tableName,
+    //     function(err, result) {
+    //         if (err) {
+    //             console.log("ERROR:" + err)
+    //         };
+    //         callback();
+    //     });
+};
+
+
+JDBCdown.prototype._get = function(key, options, cb) {
+    var self = this;
+    var asBuffer = true;
+    if (options.asBuffer === false) {
+        asBuffer = false;
+    }
+    if (options.raw) {
+        asBuffer = false;
+    }
+    key = util.encode(key);
+
+    this.pool.execute("SELECT V FROM " + this.tablename + " WHERE K = ?", [key], function(err, res, rows) {
+        if (err) {
+            return cb(err.stack);
+        }
+        if (rows === undefined) {
+            return cb(new Error('NotFound'));
+        }
+        if (rows.length === 0) {
+            return cb(new Error('NotFound'));
+        }
+
+        try {
+            rows.forEach(function(row) {
+                var value = row.V;
+
+                if (value === undefined || value === null) {
+                    return cb(new Error('NotFound'));
+                }
+                // protoect
+                var decodedVal = util.decode(value, asBuffer, true);
+                cb(null, decodedVal);
+            });
+        } catch (e) {
+            console.log(e);
+            cb(new Error('NotFound'));
+        }
+    });
+}
+
+JDBCdown.prototype._put = function(key, value, opt, cb) {
+    var self = this;
+    value = util.encode(value, true);
+    key = util.encode(key);
+
+    insertHelper(this.pool, cb, key, value);
+}
+
+
+JDBCdown.prototype._del = function(key, opt, cb) {
+    var self = this;
+    key = util.encode(key);
+    deleteHelper(this.pool, cb, key);
+}
+
+function unique(array) {
+    var things = {};
+    array.forEach(function(item) {
+        things[item.key] = item;
+    });
+    return Object.keys(things).map(function(key) {
+        return things[key];
+    });
+}
+
+JDBCdown.prototype._batch = function(array, options, callback) {
+    var inserts = 0;
+      this.pool.beginTransaction(function(err, tran) {
+          return Promise.all(unique(array).map(function(item) {
+              var key = util.encode(item.key);
+
+              if (item.type === 'del') {
+                  return deleteHelper(tran, function() {}, key);
+              } else {
+                  var value = util.encode(item.value, true);
+                  inserts++;
+                  return insertHelper(tran, function() {}, key, value);
+              }
+          })).then(function() {
+              tran.commit(function(err) {
+                  if (err) {
+                      console.log(err)
+                  }
+              });
+              callback();
+          });
+      });
+}
+
+
+JDBCdown.prototype._close = function(callback) {
+    console.log('closing connection');
+    this.pool.close();
+};
+
+JDBCdown.prototype.iterator = function(options) {
+    return new Iter(this, options);
+};
+
+function connection(url, user, password) {
+    console.log('connecting...')
+    return new jdbc.Database({
+        url: url,
+        properties: {
+            user: user,
+            password: password,
+        },
+        minConnections: 1,
+        maxConnections: 30,
+        idleTimeout: 60
+    });
+}
+
+function insertHelper(db, cb, key, value) {
+    db.execute("INSERT INTO " + TABLENAME + " (K, V) VALUES (?,?) ON DUPLICATE KEY UPDATE V=?", [key, value, value], function(err) {
+        if (err) {
+            console.log(err)
+        }
+        cb();
+    });
+}
+
+function deleteHelper(db, cb, key, value) {
+    db.execute("DELETE FROM " + TABLENAME + " where K=?", [key], function(err) {
+        if (err) {
+            console.log(err);
+        }
+
+        cb();
+    });
+}
---
+++ db/jdbcdown/iterator.js
@@ -0,0 +1,216 @@
+'use strict';
+var inherits = require('inherits');
+var AbstractIterator = require('abstract-leveldown/abstract-iterator');
+var util = require('./encoding');
+var PassThrough = require('stream').PassThrough;
+
+function goodOptions(opts, name) {
+  if (!(name in opts)) {
+    return;
+  }
+  var thing = opts[name];
+  if (thing === null) {
+    delete opts[name];
+    return;
+  }
+  if (Buffer.isBuffer(thing) || typeof thing === 'string') {
+    if (!thing.length) {
+      delete opts[name];
+      return;
+    }
+
+    opts[name] = util.encode(thing);
+  }
+}
+
+function Iterator(db, options, cb) {
+  AbstractIterator.call(this, db);
+  options = options || {};
+  this._order = !options.reverse;
+  this._options = options;
+  names.forEach(function (i) {
+    goodOptions(options, i);
+  });
+
+  this._stream  = new PassThrough({
+    objectMode: true
+  })
+
+  this._stream.once('end', function() {
+    self._endEmitted = true
+  })
+
+  this._count = 0;
+  var self = this;
+  if ('limit' in options) {
+    this._limit = options.limit;
+  } else {
+    this._limit = -1;
+  }
+
+  if ('keyAsBuffer' in options) {
+    this._keyAsBuffer = options.keyAsBuffer;
+  } else {
+    this._keyAsBuffer = true;
+  }
+  if ('valueAsBuffer' in options) {
+    this._valueAsBuffer = options.valueAsBuffer;
+  } else {
+    this._valueAsBuffer = true;
+  }
+
+  var statement = this.buildSQL();
+  if (this._limit === 0) {
+    this._next = function (cb) {
+      process.nextTick(cb);
+    };
+    return;
+  }
+
+  var __stream = this._stream;
+
+  // TODO: trireme-jdbc has executeStream, which doesn't seem to work.
+  // best to use an actual stream, or add a limit to fetched rows
+  db.pool.execute(statement.sql, statement.args,
+    function(err, result, rows) {
+      var ended =false;
+      if ((!rows) || (rows.length == 0))
+      {
+        __stream.end();
+      }
+      else
+      {
+        rows.forEach(function(row) {
+          __stream.write(row, function(){if (ended){__stream.end();}});
+        });
+      }
+      ended = true;
+    });
+}
+
+inherits(Iterator, AbstractIterator);
+module.exports = Iterator;
+var names = [
+  'start',
+  'end',
+  'gt',
+  'gte',
+  'lt',
+  'lte'
+];
+
+Iterator.prototype._next = function(callback) {
+  var self = this
+    , obj = this._stream.read()
+    , onReadable = function() {
+        self._stream.removeListener('end', onEnd)
+        self._next(callback)
+      }
+    , onEnd = function() {
+        self._stream.removeListener('readable', onReadable)
+        callback()
+      }
+    , key
+    , value
+
+  if (this._endEmitted)
+    callback()
+  else if (obj === null) {
+    this._stream.once('readable', onReadable)
+
+    this._stream.once('end', onEnd)
+  }
+  else {
+    key = obj.K
+    if (!this._keyAsBuffer) key = key.toString()
+
+    value = obj.V
+    if (!this._valueAsBuffer) value = value.toString()
+
+    callback(null, key, value)
+  }
+}
+
+Iterator.prototype._end = function(callback) {
+  this._stream.end()
+  callback()
+}
+
+Iterator.prototype.buildSQL = function () {
+  var self = this;
+  var sql = "select K, V from " + this.tableName + " where ";
+  var args = [];
+  var statement = {sql: sql, args: args};
+
+  if (this._order) {
+    if ('start' in this._options) {
+      if (this._options.exclusiveStart) {
+        if ('start' in this._options) {
+          this._options.gt = this._options.start;
+        }
+      } else {
+        if ('start' in this._options) {
+          this._options.gte = this._options.start;
+        }
+      }
+    }
+    if ('end' in this._options) {
+      this._options.lte = this._options.end;
+    }
+  } else {
+    if ('start' in this._options) {
+      if (this._options.exclusiveStart) {
+        if ('start' in this._options) {
+          this._options.lt = this._options.start;
+        }
+      } else {
+        if ('start' in this._options) {
+          this._options.lte = this._options.start;
+        }
+      }
+    }
+    if ('end' in this._options) {
+      this._options.gte = this._options.end;
+    }
+  }
+
+  if ('lt' in this._options) {
+    appendWhere(statement, 'K <', this._options.lt);
+  }
+  if ('lte' in this._options) {
+   appendWhere(statement, 'K <=', this._options.lte);
+  }
+  if ('gt' in this._options) {
+   appendWhere(statement, 'K >', this._options.gt);
+  }
+  if ('gte' in this._options) {
+   appendWhere(statement, 'K >=', this._options.gte);
+  }
+
+  statement.sql += " 1=1 ";
+
+  if (this._order) {
+    appendOrderBy(statement, true);
+  }
+  else {
+    appendOrderBy(statement, false);
+  }
+
+  if (this._limit > 0) {
+    statement.sql = statement.sql + " limit ?";
+     statement.args.push(this._limit);
+  }
+
+  return statement;
+};
+
+function appendWhere(statement, condition, appendee)
+{
+  statement.sql += (condition + " '" + util.decode(appendee) + "' AND ");
+}
+
+function appendOrderBy(statement, bool) {
+  statement.sql += " order by K " + (bool? "ASC" : "DESC");
+}
+
+module.exports = Iterator;
