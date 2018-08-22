9f0f924fcadd4a65c3dfd52a15b38ecc229ac4c8
Amit Hurvitz <hurvitz.amit@gmail.com>
2018-08-22 12:35:56 +0300

    format sql-flavour.js

--- db/jdbcdown/sql-flavour.js
+++ db/jdbcdown/sql-flavour.js
@@ -4,73 +4,73 @@ var sqlFlavour;
 
 function setSqlFlavourByJdbcUrl(jdbcUrl)
 {
-    if ((jdbcUrl.startsWith("jdbc:h2")) ||
-        (jdbcUrl.startsWith("jdbc:mysql"))) {
-        sqlFlavour = "mysql";
-    }
-    else {
-        sqlFlavour = "postgres";
-    }
+	if ((jdbcUrl.startsWith("jdbc:h2")) ||
+		(jdbcUrl.startsWith("jdbc:mysql"))) {
+		sqlFlavour = "mysql";
+	}
+	else {
+		sqlFlavour = "postgres";
+	}
 }
 
 function getDBEngineDefinition()
 {
-    switch (sqlFlavour) {
-        case "mysql":
-            return 'ENGINE=InnoDB';
-        case "postgres":
-	    return '';
-        default:
-            return '';
+	switch (sqlFlavour) {
+		case "mysql":
+ 			return 'ENGINE=InnoDB';
+		case "postgres":
+			return '';
+		default:
+			return '';
     }
 }
 
 function sqlForOnDuplicateKey(constraintName)
 {
-    switch (sqlFlavour) {
-        case "mysql":
-            return "ON DUPLICATE KEY UPDATE";
-        case "postgres":
-            return 'ON CONFLICT ON CONSTRAINT ' + constraintName + ' DO UPDATE SET';
-        default:
-	    return '';
+	switch (sqlFlavour) {
+		case "mysql":
+			return "ON DUPLICATE KEY UPDATE";
+		case "postgres":
+			return 'ON CONFLICT ON CONSTRAINT ' + constraintName + ' DO UPDATE SET';
+		default:
+			return '';
     }
 }
 
 function blobType()
 {
-    switch (sqlFlavour) {
-        case "mysql":
-            return "BLOB";
-        case "postgres":
-            return "TEXT";
-        default:
-            return "";
-    }
+	switch (sqlFlavour) {
+		case "mysql":
+			return "BLOB";
+		case "postgres":
+			return "TEXT";
+		default:
+			return "";
+	}
 }
 
 function isUsingEncoding()
 {
 	switch (sqlFlavour) {
-        case "mysql":
-            return true;
-        case "postgres":
-            return false;
-        default:
-            return true;
+		case "mysql":
+			return true;
+		case "postgres":
+			return false;
+		default:
+			return true;
     }
 }
 
 function fieldName(name)
 {
 	switch (sqlFlavour) {
-        case "mysql":
-            return name.toUpperCase();
-        case "postgres":
-            return name.toLowerCase();
-        default:
-            return name;
-    }
+		case "mysql":
+			return name.toUpperCase();
+		case "postgres":
+			return name.toLowerCase();
+		default:
+			return name;
+	}
 }
 
 exports.setSqlFlavourByJdbcUrl = setSqlFlavourByJdbcUrl;
