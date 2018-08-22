878bf1ce47fa6ecc2a6defbc1c52a083c6847ab3
Amit Hurvitz <hurvitz.amit@gmail.com>
2018-08-22 12:31:16 +0300

    add sql-flavour.js

---
+++ db/jdbcdown/sql-flavour.js
@@ -0,0 +1,81 @@
+'use strict';
+
+var sqlFlavour;
+
+function setSqlFlavourByJdbcUrl(jdbcUrl)
+{
+    if ((jdbcUrl.startsWith("jdbc:h2")) ||
+        (jdbcUrl.startsWith("jdbc:mysql"))) {
+        sqlFlavour = "mysql";
+    }
+    else {
+        sqlFlavour = "postgres";
+    }
+}
+
+function getDBEngineDefinition()
+{
+    switch (sqlFlavour) {
+        case "mysql":
+            return 'ENGINE=InnoDB';
+        case "postgres":
+	    return '';
+        default:
+            return '';
+    }
+}
+
+function sqlForOnDuplicateKey(constraintName)
+{
+    switch (sqlFlavour) {
+        case "mysql":
+            return "ON DUPLICATE KEY UPDATE";
+        case "postgres":
+            return 'ON CONFLICT ON CONSTRAINT ' + constraintName + ' DO UPDATE SET';
+        default:
+	    return '';
+    }
+}
+
+function blobType()
+{
+    switch (sqlFlavour) {
+        case "mysql":
+            return "BLOB";
+        case "postgres":
+            return "TEXT";
+        default:
+            return "";
+    }
+}
+
+function isUsingEncoding()
+{
+	switch (sqlFlavour) {
+        case "mysql":
+            return true;
+        case "postgres":
+            return false;
+        default:
+            return true;
+    }
+}
+
+function fieldName(name)
+{
+	switch (sqlFlavour) {
+        case "mysql":
+            return name.toUpperCase();
+        case "postgres":
+            return name.toLowerCase();
+        default:
+            return name;
+    }
+}
+
+exports.setSqlFlavourByJdbcUrl = setSqlFlavourByJdbcUrl;
+exports.getDBEngineDefinition = getDBEngineDefinition;
+exports.sqlForOnDuplicateKey = sqlForOnDuplicateKey;
+exports.blobType = blobType;
+exports.isUsingEncoding = isUsingEncoding;
+exports.fieldName = fieldName;
